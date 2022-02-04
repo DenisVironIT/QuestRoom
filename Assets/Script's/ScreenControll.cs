1d250c8c4f17df5c4b687825d5c13dafad52eecc
DenisVironIT <d.kryuchkov.vironit@vironit.ru>
2022-02-04 16:59:55 +0300

    cinema machine

--- Assets/Script's/ScreenControll.cs
+++ Assets/Script's/ScreenControll.cs
@@ -9,83 +9,102 @@ using UnityEngine.UI;
 public class ScreenControll : MonoBehaviour
 {
     [SerializeField] private TMP_InputField input;
-	public Animator giveKey;
-	[SerializeField] GameObject monitor;
-	[SerializeField] GameObject playControl;
-	public bool turn;
-	public Transform Player;
+    public Animator giveKey;
+    [SerializeField] GameObject cameraPlayer;
+    [SerializeField] GameObject cameraNotebook;
+    [SerializeField] GameObject monitor;
+    [SerializeField] GameObject playControl;
+    public bool turn;
+    public Transform Player;
 
-	void Start()
-	{
-		turn = false;
-	}
+    void Start()
+    {
+        turn = false;
+    }
 
-	void OnMouseOver()
-	{
-		{
-			if (input.text == "1234" && Input.GetKeyDown(KeyCode.F))
-			{
-				print("Unlock");
-				giveKey.Play("Open");
-			}
-			if (Player)
-			{
-				float dist = Vector3.Distance(Player.position, transform.position);
-				if (dist < 2)
-				{
-					if (turn == false)
-					{
-						if (Input.GetKeyDown(KeyCode.E))
-						{
-							StartCoroutine(opening());
-						}
-					}
-					else
-					{
-						if (turn == true)
-						{
-							if (Input.GetKeyDown(KeyCode.E))
-							{
-								StartCoroutine(closing());
-							}
-						}
 
-					}
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            monitor.SetActive(true);
+      
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+            monitor.SetActive(false);
+    }
 
-				}
-			}
+    void OnMouseOver()
+    {
+        {
 
-		}
+            if (input.text == "1234")
+            {
+                print("Unlock");
+                giveKey.Play("Open");
+            }
+            if (Player)
+            {
+                float dist = Vector3.Distance(Player.position, transform.position);
+                if (dist < 1.2)
+                {
+                    if (turn == false)
+                    {
+                        if (Input.GetKeyDown(KeyCode.E))
+                        {
+                            StartCoroutine(opening());
+                        }
+                    }
+                    else
+                    {
+                        if (turn == true)
+                        {
+                            if (Input.GetKeyDown(KeyCode.E))
+                            {
+                                StartCoroutine(closing());
+                            }
+                        }
 
-	}
+                    }
 
-	IEnumerator opening()
-	{
-		print("nootebook turn on");
-		monitor.SetActive(true);
-		playControl.GetComponent<FirstPersonController>().enabled = false;
-		Camera.main.fieldOfView = 30;
-		Cursor.visible = true;
-		Cursor.lockState = CursorLockMode.None;
-		
+                }
+            }
 
-		turn = true;
-		yield return new WaitForSeconds(.5f);
-	}
+        }
 
-	IEnumerator closing()
-	{
-		print("nootebook turn off");
-		monitor.SetActive(false);
-		playControl.GetComponent<FirstPersonController>().enabled = true;
-		Cursor.visible = false;
-		Cursor.lockState = CursorLockMode.Locked;
-		turn = false;
-		yield return new WaitForSeconds(.5f);
-	}
+    }
+
+    IEnumerator opening()
+    {
+        print("nootebook turn on");
+
+        playControl.GetComponent<FirstPersonController>().enabled = false;
+        cameraPlayer.SetActive(false);
+        cameraNotebook.SetActive(true);
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+
+
+        turn = true;
+        yield return new WaitForSeconds(.5f);
+    }
+
+    IEnumerator closing()
+    {
+        print("nootebook turn off");
+
+        playControl.GetComponent<FirstPersonController>().enabled = true;
+        cameraPlayer.SetActive(true);
+        cameraNotebook.SetActive(false);
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        turn = false;
+        yield return new WaitForSeconds(.5f);
+    }
 
 
 }
-    
+
 
 
